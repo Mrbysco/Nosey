7ee90c16bac8d55c4f6042b1b044eeff45be4210
Mrbysco <Mrbysco@users.noreply.github.com>
2023-12-31 04:47:53 +0100

    Port to 1.20.4

--- neoforge/src/main/java/com/mrbysco/nosey/NoseyNeoForge.java
+++ neoforge/src/main/java/com/mrbysco/nosey/NoseyNeoForge.java
@@ -7,14 +7,12 @@ import net.neoforged.fml.IExtensionPoint;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.neoforged.fml.loading.FMLEnvironment;
 
 @Mod(Constants.MOD_ID)
 public class NoseyNeoForge {
 
-	public NoseyNeoForge() {
-		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+	public NoseyNeoForge(IEventBus eventBus) {
 		//Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
 		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () ->
 				new IExtensionPoint.DisplayTest(() -> "Trans Rights Are Human Rights",
